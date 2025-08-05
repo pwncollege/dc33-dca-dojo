dc2b86ff54990c7461c0d0c2feac0e76480258e1
rh4hunnid <rh4@keemail.me>
2025-08-04 19:27:37 -0700

    Updated Challenge Files

--- dc33-byte-sized-basics/chals/asm/string.asm
+++ dc33-byte-sized-basics/chals/asm/string.asm
@@ -3,18 +3,33 @@ global main
 extern printf
 
 section .data
-    fmt:    db "%s", 10, 0    ; “%s” plus real LF
+    fmt:    db "%s", 10, 0
+    fmt1:   db  "rsp = %llx", 10, 0
 
 section .text
+
+print_rsp:
+  sub rsp, 8
+  mov rdi,fmt1
+  mov rsi, rsp
+  xor rax,rax
+  call printf
+  add rsp,8
+  ret
+
 main:
 
-<YOUR CODE GOES HERE>
+;<YOUR CODE GOES HERE>
 
 
+;<YOUR CODE STOPS HERE>
 
 
-<YOUR CODE STOPS HERE>
-
+lea rdi, [rel fmt]
+mov rsi, rsp
+xor eax, eax
+call printf
+add rsp, 8
 
 mov rax, 60
 syscall
